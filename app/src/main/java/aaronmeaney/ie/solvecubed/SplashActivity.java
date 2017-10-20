3f5b1d808eab9339b68bc2f71fb83e4afeab25c9
Reccy <aaronmeaney@hotmail.com>
2017-10-20 17:36:25 +0100

    Changed Splash Screen to be less abitrary. Splash Screen and Main Menu are now the same activity. Reverted to Java 7.

--- app/src/main/java/aaronmeaney/ie/solvecubed/SplashActivity.java
+++ app/src/main/java/aaronmeaney/ie/solvecubed/SplashActivity.java
@@ -1,9 +1,7 @@
 package aaronmeaney.ie.solvecubed;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 public class SplashActivity extends Activity {
 
@@ -11,23 +9,5 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        startMainMenuActivity(5);
-    }
-
-    /**
-     * Loads the main menu activity after duration 'loadingTimeSeconds' elapses and
-     * then finishes the splash screen activity
-     */
-    private void startMainMenuActivity(int delaySeconds)
-    {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent mainMenuIntent = new Intent(SplashActivity.this, MainMenuActivity.class);
-                startActivity(mainMenuIntent);
-                finish();
-            }
-        }, delaySeconds * 1000);
     }
 }
