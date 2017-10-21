5cfd0820e1e1fe798d67419e4b12dcdb6390e071
Reccy <aaronmeaney@hotmail.com>
2017-10-21 20:04:46 +0100

    Updated buttons on main menu.

--- app/src/main/java/aaronmeaney/ie/solvecubed/SplashActivity.java
+++ app/src/main/java/aaronmeaney/ie/solvecubed/SplashActivity.java
@@ -2,12 +2,25 @@ package aaronmeaney.ie.solvecubed;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Intentional UI blocking code to simulate arbitrary splash screen, only runs in release mode
+        if (!BuildConfig.DEBUG)
+        {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                System.out.print(e);
+                System.exit(1);
+            }
+        }
+
         setContentView(R.layout.activity_splash);
     }
 }
