96af0a2b0082600c57bed7aecd6ec4f82e51388e
Reccy <aaronmeaney@hotmail.com>
2017-10-31 22:23:04 +0000

    Added more icons to color calibration screen. Changed color calibration screen to AppCompatActivity to allow compatibility with FABs.

--- app/src/main/java/ie/aaronmeaney/solvecubed/ColorCalibratorCameraActivity.java
+++ app/src/main/java/ie/aaronmeaney/solvecubed/ColorCalibratorCameraActivity.java
@@ -1,10 +1,12 @@
 package ie.aaronmeaney.solvecubed;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceView;
+import android.view.View;
 
-public class ColorCalibratorCameraActivity extends Activity {
+public class ColorCalibratorCameraActivity extends AppCompatActivity {
 
     // The output view for the camera
     SurfaceView cameraOutput;
@@ -15,17 +17,29 @@ public class ColorCalibratorCameraActivity extends Activity {
     // The ID of the back camera
     String backCameraId;
 
+    // The FAB for the camera
+    FloatingActionButton capturePhotoButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_calibrator_camera);
 
         cameraOutput = findViewById(R.id.surface_color_calibrator);
+        capturePhotoButton = findViewById(R.id.camera_color_capture_button);
 
         // Make Camera render to the SurfaceView
         simpleCameraManager = new SimpleCameraManager(ColorCalibratorCameraActivity.this);
         backCameraId = simpleCameraManager.getBackCameraId();
         simpleCameraManager.streamCameraToTexture(backCameraId, cameraOutput.getHolder().getSurface());
+
+        // Setup capture button
+        capturePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Click!");
+            }
+        });
     }
 
     @Override
