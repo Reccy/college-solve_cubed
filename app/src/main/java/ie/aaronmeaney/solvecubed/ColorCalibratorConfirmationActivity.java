c79365c0b3a19706c617f4899cb88df877d8445a
Reccy <aaronmeaney@hotmail.com>
2017-11-01 18:13:50 +0000

    Added help menu to color calibration confirmation activity. Changed default style to appcompat.

--- app/src/main/java/ie/aaronmeaney/solvecubed/ColorCalibratorConfirmationActivity.java
+++ app/src/main/java/ie/aaronmeaney/solvecubed/ColorCalibratorConfirmationActivity.java
@@ -2,13 +2,36 @@ package ie.aaronmeaney.solvecubed;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class ColorCalibratorConfirmationActivity extends Activity {
+public class ColorCalibratorConfirmationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_calibrator_confirmation);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.color_picker, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_help:
+                Toast.makeText(this, R.string.help_menu_placeholder, Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
