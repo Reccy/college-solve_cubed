66401f4ff1a1dfb246730c43db6181044b8918ef
Reccy <aaronmeaney@hotmail.com>
2017-11-03 22:40:24 +0000

    Added text to speech engine for help menu actions.

--- app/src/main/java/ie/aaronmeaney/solvecubed/SolveCubedAppCompatActivity.java
+++ app/src/main/java/ie/aaronmeaney/solvecubed/SolveCubedAppCompatActivity.java
@@ -1,12 +1,17 @@
 package ie.aaronmeaney.solvecubed;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 import ie.aaronmeaney.solvecubed.R;
 
@@ -14,6 +19,16 @@ import ie.aaronmeaney.solvecubed.R;
  * AppCompatActivity with pre-configured menu for the app
  */
 public abstract class SolveCubedAppCompatActivity extends AppCompatActivity {
+
+    /**
+     * The text to speech engine used for the help menu
+     */
+    private TextToSpeech speechEngine;
+
+    /**
+     * The help dialogue written by the help menu
+     */
+    private String helpDialogue;
 
     /**
      * Reference to this activity used by anonymous classes
@@ -25,6 +40,16 @@ public abstract class SolveCubedAppCompatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         thisActivity = this;
+        helpDialogue = getResources().getString(R.string.menu_action_placeholder);
+
+        speechEngine = new TextToSpeech(thisActivity, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    speechEngine.setLanguage(Locale.UK);
+                }
+            }
+        });
     }
 
     /**
@@ -55,6 +80,21 @@ public abstract class SolveCubedAppCompatActivity extends AppCompatActivity {
      * Class to be overridden in concrete classes to handle the Help action.
      */
     protected void onHelpMenuActionSelected() {
-        Toast.makeText(this, R.string.menu_action_placeholder, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, helpDialogue, Toast.LENGTH_LONG).show();
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isSpeechEnabled = prefs.getBoolean(getString(R.string.pref_text_speech_enabled_key), false);
+
+        if(isSpeechEnabled) {
+            speak(helpDialogue);
+        }
+    }
+
+    /**
+     * Uses the text-to-speech engine to speak the passed in dialogue
+     * @param dialogue What to speak
+     */
+    private void speak(String dialogue) {
+        speechEngine.speak(dialogue, TextToSpeech.QUEUE_FLUSH, null);
     }
 }
